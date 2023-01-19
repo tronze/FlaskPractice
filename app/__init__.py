9d2f3bf834693320ccc097cddbeaebaa224cfea5
tronze <0183535663@naver.com>
2023-01-19 19:53:29 +0900

    Add secret key to use session.

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask import Flask
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY=')H@McQfTjWnZr4t7w!z%C*F-JaNdRgUkXp2s5v8x/A?D(G+KbPeShVmYq3t6w9z$'
+    )
 
     from app import database
     database.init_app(app)
