11594a014009c9fce4651ccd319023bb0fb135cf
tronze <0183535663@naver.com>
2023-01-19 18:19:07 +0900

    Add database initialization when starting app.

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,8 @@ from flask import Flask
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
+
+    from app import database
+    database.init_app(app)
+
     return app
