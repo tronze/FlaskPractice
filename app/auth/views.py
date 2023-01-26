62e87368ddd3629915c0bb93332ea112b42def03
tronze <0183535663@naver.com>
2023-01-26 15:38:19 +0900

    Load user object into g before handling request.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,22 @@
-from flask import Blueprint, request, render_template, abort, redirect, url_for, session
+from flask import Blueprint, render_template, abort, redirect, url_for, session, g
 
 from app.auth.forms import LoginForm, RegisterForm
 from app.auth.models import User
 from app.database import db_session
 
 bp = Blueprint('auth', __name__, url_prefix='/auth', template_folder='templates/auth')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    """If a user id is stored in the session, load the user object from
+    the database into ``g.user``."""
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
 
 
 @bp.route('/login', methods=('GET', 'POST'))
